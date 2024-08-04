9b55c554af34b446f707b1d341fe768f00af4566
keanutaufan <keanu.taufan21@gmail.com>
2024-08-04 14:06:48 +0700

    Implement footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__paragraph">Keanu Taufan's personal website. Find this interesting? <a href="" className="footer__link">Contact Me</a></p>
+    </div>
+  );
+}
